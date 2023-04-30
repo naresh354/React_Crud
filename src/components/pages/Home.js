7fe91ecfc22d76f449c41bf588f3ef434911b59c
winterwilliamson <wintersoldier354@gmail.com>
2023-04-30 22:52:42 +0530

    implemented initial commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const [users, setUser] = useState([]);
+
+  useEffect(() => {
+    loadUsers();
+  }, [users]);
+
+  const loadUsers = async () => {
+    const result = await axios.get("https://dummy.restapiexample.com/api/v1/employees");
+    setUser(result?.data?.data);
+  };
+
+  const deleteUser = async id => {
+    await axios.delete(`https://dummy.restapiexample.com/api/v1/delete/${id}`);
+    loadUsers();
+  };
+
+  console.log(users, "users")
+
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>Home Page</h1>
+        <table class="table border shadow">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Salery</th>
+              <th scope="col">Age</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{user.employee_name}</td>
+                <td>{user.employee_salary}</td>
+                 <td>{user.employee_age}</td>
+                
+                <td>
+                  <Link class="btn btn-primary mr-2" to={`/users/${user.id}`}>
+                    View
+                  </Link>
+                  <Link
+                    class="btn btn-outline-primary mr-2"
+                    to={`/users/edit/${user.id}`}
+                  >
+                    Edit
+                  </Link>
+                  <Link
+                    class="btn btn-danger"
+                    onClick={() => deleteUser(user.id)}
+                  >
+                    Delete
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
